8c0d2c88f177c7f2bb844ab99648dbcf4e8ea7f9
GorHovsepyann <146917478+GorHovsepyann@users.noreply.github.com>
2024-05-30 12:47:05 +0400

    v2

--- main/admin.py
+++ main/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 from .models import category,subcategory
-from unfold.admin import ModelAdmin
 # Register your models here.
 
-    
-    
-@admin.register(category)
-class CategoryAdmin(ModelAdmin):
-    pass
 
-@admin.register(subcategory)
-class SubcategoryAdmin(ModelAdmin):
-    pass
-# admin.site.register(category)
-# admin.site.register(subcategory)
-
+admin.site.register(category)
+admin.site.register(subcategory)
