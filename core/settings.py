350a82bf4c1d2f09f61538334cc4603a7a8ab270
GorHovsepyann <146917478+GorHovsepyann@users.noreply.github.com>
2024-05-30 12:46:28 +0400

    v2

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "unfold",
+    # "unfold",
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
